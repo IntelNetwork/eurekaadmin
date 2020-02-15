c602989861ea9a568cfda5116944ffd8836e3b44
Hank.huang <Hank.huang@shdbr.com>
2020-02-15 15:21:08 +0800

    'eureka注册中心'

---
+++ src/main/java/com/smartwork/SmartworkServerApplication.java
@@ -0,0 +1,14 @@
+package com.smartwork;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SmartworkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartworkServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartwork/smartwork_server/SmartworkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartwork.smartwork_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartworkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
